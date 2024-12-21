1fea2d5144df0ebe29f1d17e2a3768ba6d3be177
hkr <149226162+White-Darkness@users.noreply.github.com>
2024-12-21 18:41:05 +0530

    Update EditorPage.js
    made compiler working

--- src/components/EditorPage.js
+++ src/components/EditorPage.js
@@ -105,7 +105,7 @@ function EditorPage() {
   const runCode = async () => {
     setIsCompiling(true);
     try {
-      const response = await axios.post("http://localhost:5000/compile", {
+      const response = await axios.post("https://server-7dhp.onrender.com/compile", {
         code: codeRef.current,
         language: selectedLanguage,
       });
